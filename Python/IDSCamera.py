37f26c8fe5896bfd1b967d2762fd294776fe7188
yangmh <fastyangmh@gmail.com>
2022-02-23 11:57:25 +0800

    rewrite IDSCamera.py.

--- Python/IDSCamera.py
+++ Python/IDSCamera.py
@@ -1,12 +1,11 @@
 '''
 the following link is online manuals from IDS
-https://cn.ids-imaging.com/manuals/ids-software-suite/ueye-manual/4.94/zh/is_camerastatus.html
+https://cn.ids-imaging.com/manuals/ids-software-suite/ueye-manual/4.94/zh/sdk-programming-image-capture-modes.html
 '''
 
 # import
 import configparser
 from pyueye import ueye
-from ctypes import c_uint, c_wchar_p
 import numpy as np
 import matplotlib.pyplot as plt
 from os.path import isfile
@@ -16,9 +15,9 @@ import cv2
 
 
 class IDSCamera:
-    def __init__(self, camera_parameter_path):
+    def __init__(self, parameter_path):
         # table
-        self.color_modes_table = {
+        self.color_mode_table = {
             '6': 'ueye.IS_CM_MONO8',
             '0': 'ueye.IS_CM_BGRA8_PACKED',
             '1': 'ueye.IS_CM_BGR8_PACKED'
@@ -30,74 +29,75 @@ class IDSCamera:
         }
 
         # get parameters from config
-        self.config = self.load_ini_file(
-            camera_parameter_path=camera_parameter_path)
+        self.config = self.load_ini_file(parameter_path=parameter_path)
         self.width = int(self.config['Image size']['Width'])
         self.height = int(self.config['Image size']['Height'])
         self.bitspixel = self.bits_of_pixel_table[self.config['Parameters']
                                                   ['Colormode']]
         self.lineinc = self.width * int((self.bitspixel + 7) / 8)
         self.channels = int(np.ceil((self.bitspixel / 8)))
-        self.color_mode = self.color_modes_table[self.config['Parameters']
-                                                 ['Colormode']]
+        self.color_mode = self.color_mode_table[self.config['Parameters']
+                                                ['Colormode']]
         self.color_order = ''.join(
             filter(lambda x: not x.isdigit(),
                    self.color_mode.split('IS_CM_')[1].split('_')[0]))
         self.color_code = self.color_code_table.get(self.color_order)
 
         # init camera
-        self.camera = ueye.HIDS(0)
-        result = ueye.is_InitCamera(phCam=self.camera, hWnd=None)
-        self.check(result=result, info='initial')
+        self.camera = ueye.c_uint(
+        )  #TODO: need to check if ueye.HIDS can select camera based on number
+        self.hWnd = ueye.c_void_p()
+        result = ueye.is_InitCamera(phCam=self.camera, hWnd=self.hWnd)
+        self.check(result=result, info='is_InitCamera')
 
         # set color mode
         result = ueye.is_SetColorMode(hCam=self.camera,
                                       Mode=eval(self.color_mode))
-        self.check(result=result, info='set color mode')
+        self.check(result=result, info='is_SetColorMode')
 
         # load parameter
         result = ueye.is_ParameterSet(
             hCam=self.camera,
             nCommand=ueye.IS_PARAMETERSET_CMD_LOAD_FILE,
-            pParam=c_wchar_p(camera_parameter_path),
-            cbSizeOfParam=c_uint(0))
-        self.check(result=result, info='load parameter')
+            pParam=ueye.c_wchar_p(parameter_path),
+            cbSizeOfParam=ueye.c_uint())
+        self.check(result=result, info='is_ParameterSet')
 
         # allocate memory
-        self.mem_ptr = ueye.c_mem_p()
-        mem_id = ueye.int()
+        self.ppcMem = ueye.c_mem_p()
+        self.pnMemId = ueye.c_int()
         result = ueye.is_AllocImageMem(hCam=self.camera,
                                        width=self.width,
                                        height=self.height,
                                        bitspixel=self.bitspixel,
-                                       ppcImgMem=self.mem_ptr,
-                                       pid=mem_id)
-        self.check(result=result, info='allocate memory')
+                                       ppcMem=self.ppcMem,
+                                       pnMemId=self.pnMemId)
+        self.check(result=result, info='is_AllocImageMem')
 
         # set active memory region
         result = ueye.is_SetImageMem(hCam=self.camera,
-                                     pcMem=self.mem_ptr,
-                                     id=mem_id)
-        self.check(result=result, info='set active memory region')
+                                     pcMem=self.ppcMem,
+                                     nMemId=self.pnMemId)
+        self.check(result=result, info='is_SetImageMem')
 
         # set DIB
         result = ueye.is_SetDisplayMode(hCam=self.camera,
                                         Mode=ueye.IS_SET_DM_DIB)
-        self.check(result=result, info='set DIB')
+        self.check(result=result, info='is_SetDisplayMode')
 
-    def load_ini_file(self, camera_parameter_path):
+    def load_ini_file(self, parameter_path):
         config = configparser.ConfigParser()
-        config.read(camera_parameter_path, 'utf-8-sig')
+        config.read(parameter_path, 'utf-8-sig')
         return config
 
     def check(self, result, info):
-        assert result == ueye.IS_SUCCESS, 'the camera does not successful {}, the return code {}.'.format(
+        assert result == ueye.IS_SUCCESS, 'the camera does not successful {}, the return code is {}.'.format(
             info, result)
 
     def __call__(self):
         result = ueye.is_FreezeVideo(hCam=self.camera, Wait=ueye.IS_WAIT)
-        self.check(result=result, info='freeze video')
-        image = ueye.get_data(image_mem=self.mem_ptr,
+        self.check(result=result, info='is_FreezeVideo')
+        image = ueye.get_data(image_mem=self.ppcMem,
                               x=self.width,
                               y=self.height,
                               bits=self.bitspixel,
@@ -114,20 +114,23 @@ class IDSCamera:
         return image
 
     def release(self):
+        result = ueye.is_FreeImageMem(hCam=self.camera,
+                                      pcMem=self.ppcMem,
+                                      nMemId=self.pnMemId)
+        self.check(result=result, info='is_FreeImageMem')
         result = ueye.is_ExitCamera(hCam=self.camera)
-        self.check(result=result, info='exit camera')
+        self.check(result=result, info='is_ExitCamera')
 
 
 if __name__ == '__main__':
     # parameters
-    camera_parameter_path = 'parameters.ini'
+    parameter_path = 'parameters.ini'
 
     # check camera parameter file
-    assert isfile(
-        path=camera_parameter_path), 'the camera parameters does not exist.'
+    assert isfile(path=parameter_path), 'the parameter does not exist.'
 
     # initialize camera
-    camera = IDSCamera(camera_parameter_path=camera_parameter_path)
+    camera = IDSCamera(parameter_path=parameter_path)
 
     # get image
     image = camera()
